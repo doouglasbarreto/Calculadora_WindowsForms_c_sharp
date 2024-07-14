98091aa3572eafa4341db6933aee0c31f77cd272
Douglas Barreto <doouglas_barreto@hotmail.com>
2024-07-14 19:39:04 -0300

    Criação do Projeto

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Calculadora_WindowsForms_c_sharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
